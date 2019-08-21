37a17acb523a1c8baa0936480039570a8df4fd75
Astraliccc <53441757+Astraliccc@users.noreply.github.com>
2019-08-21 08:37:37 -0500

    Updated to v1.1.0
    - A few balance changes
    - Most UIs should now be using dark mode themes
    - Updated all mods
    - Removed a few redudant things that were content overlap
    - Removed a few mods that didn't fit the pack or had better alternatives
    - Overall things should be a lot more stable

--- .server/start.bat
+++ .server/start.bat
@@ -1,5 +1,5 @@
 @echo off
-title Mesh Server 1.0.0a
+title Mesh Server 1.1
 color 5F
 echo *******The Mesh Server is now loading*******
 java  -d64 -server -Xms6G -Xmx6G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled  -XX:MaxGCPauseMillis=100  -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -jar fabric-server-launch.jar
